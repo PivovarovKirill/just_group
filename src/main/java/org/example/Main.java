81322a5f8593c5c4490cc722f59a4e7c908a3a63
kirillpivovarov <kirilp@bandbsolution.com.ua>
2023-02-14 18:43:39 -0800

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(5,6));
+    }
+}
